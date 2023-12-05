c758a0c31390e8734409d071f082de100e1cc0ba
code-dev05 <devansh.agarwal.official@gmail.com>
2023-12-05 16:14:09 +0530

    Added dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/config.env" });
 
 import { UserRouter } from "./routes/users.js";
 import { RecipeRouter } from "./routes/recipes.js";
@@ -11,10 +14,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth", UserRouter);
-app.use("/recipes", RecipeRouter)
+app.use("/recipes", RecipeRouter);
 
 mongoose.connect(
-  "mongodb+srv://code-dev05:mernfirstproject@recipeapp.e34jyok.mongodb.net/RecipeApp?retryWrites=true&w=majority"
+  `mongodb+srv://code-dev05:${process.env.PASSWORD}@recipeapp.e34jyok.mongodb.net/RecipeApp?retryWrites=true&w=majority`
 );
 
 app.listen(3001, () => console.log("SERVER STARTED"));
